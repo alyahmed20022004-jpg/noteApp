2434ddb5a6f8c8ac440707482e4de15918bfc90e
alyahmed20022004-jpg <aly.ahmed20022004@gmail.com>
2026-04-22 16:58:22 +0200

    sono carico a molla

---
+++ noteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  noteApp
+//
+//  Created by IFTS 45 on 22/04/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
